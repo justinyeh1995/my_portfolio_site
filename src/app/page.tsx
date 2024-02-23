aa8decf440f207934f1862e55c6442b777bbe54e
justinyeh1995 <justinyeh1995@gmail.com>
2024-02-23 13:49:00 -0600

    add header & projectsection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import HeroSection from "@/components/HeroSection";
+import Header from "@/components/Header";
+import ProjectSection from "@/components/ProjectSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Header />
         <HeroSection />
+        <ProjectSection />
     </main>
   );
 }
