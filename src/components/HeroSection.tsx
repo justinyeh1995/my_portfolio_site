68faed46c983d3ea496bafa4ced8dcabb9f4df78
justinyeh1995 <justinyeh1995@gmail.com>
2024-02-23 13:49:35 -0600

    start adding personal descriptions but the layout is pretty bas rn

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function HeroSection() {
   return (
     <section className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl font-bold">Welcome to our page</h1>
-      <p className="text-2xl">This is a simple hero section</p>
+      <h1 className="text-5xl font-bold">Welcome to My page</h1>
+        <p className="text-2xl">Justin Yeh</p>
+        <p className="text-2xl">Software Engineer</p>
+        <p className="text-2xl">Hey there! I’m a creative software developer based in sunny San Francisco, CA. </p>
     </section>
   );
 }
