44a0c7f263bd57996a36cf13525f3801caf54b6c
justinyeh1995 <justinyeh1995@gmail.com>
2024-02-23 13:50:07 -0600

    update descriptions

---
+++ src/components/Project.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Project() {
+  return (
+    <section className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-5xl font-bold">My Projects</h1>
+      <p className="text-2xl">This is a simple project section</p>
+    </section>
+  );
+}
+
