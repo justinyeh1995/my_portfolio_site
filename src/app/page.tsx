a8fe770d1070e3b5e23400c4f929cf5ca0420a06
justinyeh1995 <justinyeh1995@gmail.com>
2024-05-23 11:29:38 +0800

    import updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import HeroSection from "@/components/DarkGridHero";
 import Header from "@/components/Header/Header";
-import ProjectSection from "@/components/ProjectSection";
-import Project from "@/components/Project";
-import {DarkGridHero} from "@/components/HeroSection";
+import Project from "@/components/Project/Project";
+import {DarkGridHero} from "@/components/About/HeroSection";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
         <Header />
         <DarkGridHero />
-        {/*<ProjectSection />*/}
         <Project />
     </main>
   );
