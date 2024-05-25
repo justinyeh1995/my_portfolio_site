612cfda6553ed0e42c393326ee8857eb8412c568
justinyeh1995 <justinyeh1995@gmail.com>
2024-05-25 23:11:32 +0800

    nothing done yet

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+// Footer.tsx
+
+import React from "react";
+import {motion} from "framer-motion";
+
+export const Footer = () => {
+    return (
+        <footer className="flex items-center justify-center w-full h-24 border-t">
+            <p>&copy; 2024 Justin Yeh. All rights reserved.</p>
+        </footer>
+    );
+}
