44b148fcb82cc47378dd3c8ffdf83b56fa9dcdcf
justinyeh1995 <justinyeh1995@gmail.com>
2024-05-29 02:00:12 +0800

    no flyout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -110,27 +110,27 @@ const NavLink = ({
                 {children}
                 <span
                     style={{
-                        transform: showFlyout ? "scaleX(1)" : "scaleX(0)",
+                        transform: open ? "scaleX(1)" : "scaleX(0)",
                     }}
                     className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-indigo-300 transition-transform duration-300 ease-out"
                 />
             </a>
-            <AnimatePresence>
-                {showFlyout && (
-                    <motion.div
-                        initial={{ opacity: 0, y: 15 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: 15 }}
-                        style={{ translateX: "-50%" }}
-                        transition={{ duration: 0.3, ease: "easeOut" }}
-                        className="absolute left-1/2 top-12 bg-white text-black"
-                    >
-                        <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent" />
-                        <div className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white" />
-                        <FlyoutContent />
-                    </motion.div>
-                )}
-            </AnimatePresence>
+            {/*<AnimatePresence>*/}
+            {/*    {showFlyout && (*/}
+            {/*        <motion.div*/}
+            {/*            initial={{ opacity: 0, y: 15 }}*/}
+            {/*            animate={{ opacity: 1, y: 0 }}*/}
+            {/*            exit={{ opacity: 0, y: 15 }}*/}
+            {/*            style={{ translateX: "-50%" }}*/}
+            {/*            transition={{ duration: 0.3, ease: "easeOut" }}*/}
+            {/*            className="absolute left-1/2 top-12 bg-white text-black"*/}
+            {/*        >*/}
+            {/*            <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent" />*/}
+            {/*            <div className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white" />*/}
+            {/*            <FlyoutContent />*/}
+            {/*        </motion.div>*/}
+            {/*    )}*/}
+            {/*</AnimatePresence>*/}
         </div>
     );
 };
