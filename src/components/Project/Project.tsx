e593f184fe8e0bbee7d90e851140c92cb6cc3c2c
justinyeh1995 <justinyeh1995@gmail.com>
2024-05-31 00:42:03 +0800

    fix carousel overflowing with modified css in section

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Project() {
     const [ref, { width }] = useMeasure();
     return (
-        <section id="Projects" ref={ref} className="flex min-h-screen flex-col bg-zinc-950 items-center justify-between p-24">
+        <section id="Projects" ref={ref} className="relative overflow-hidden bg-zinc-950 py-8 px-4 md:px-16 lg:px-24 min-h-screen flex flex-col justify-center">
             <div className=" overflow-hidden p-4">
                 {/* CARDS */}
                 <div className="mx-auto max-w-6xl">
