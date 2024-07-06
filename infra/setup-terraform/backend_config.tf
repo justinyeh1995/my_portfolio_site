be061e7dad38772227b95b24ea2139b3cbec72d9
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-06 18:17:19 -0500

    Modify backend_config.tf: change bucket name

--- infra/setup-terraform/backend_config.tf
+++ infra/setup-terraform/backend_config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-  bucket = "resume-frontend-terrafor-state-bucket"
+  bucket = "resume-frontend-terraform-state-bucket"
   key = "terraform.tfstate"
   region = "us-east-2"
   dynamodb_table = "terraform-state-lock"
